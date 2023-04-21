a479b7e53b1a67667d5caa1f34abdfbdf4bfd555
Evdokimov <danila.evdokimov.444@mail.ru>
2023-04-21 16:13:03 +0300

    first task

--- weather/src/main/java/com/example/weather/controller/Controller.java
+++ weather/src/main/java/com/example/weather/controller/Controller.java
@@ -1,17 +1,35 @@
 package com.example.weather.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.coyote.Request;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+
+
 
 @RestController
 @RequestMapping("/api/weather")
 public class Controller {
     private final RestTemplate restTemplate = new RestTemplate();
     @RequestMapping("get")
-    public String getWeather(@RequestParam String lat, @RequestParam String lon){
-        return "";
+    public String getWeather(@PathVariable @RequestParam String longitude, @RequestParam String latitude) throws UnirestException {
+
+        String header1 = "97979f96-acea-40bc-9ae3-bd49fc07b9ca";
+        HttpResponse httpResponse =
+                (HttpResponse<String>) Unirest.get("https://api.weather.yandex.ru/v2/forecast/?lat="+latitude+"&lon="+longitude)
+       .header("X-Yandex-API-Key", header1).asJsonAsync();
+        System.out.println( );
+
+
+        return (String) httpResponse.getBody();
     }
+
 
 }
