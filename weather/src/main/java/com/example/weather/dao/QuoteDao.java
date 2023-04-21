402f7f9b71a15c98abad35e492daf2270724cc75
desertfox <fess.2002@mail.ru>
2023-04-21 16:10:24 +0300

    Quotes ready

--- weather/src/main/java/com/example/weather/dao/QuoteDao.java
+++ weather/src/main/java/com/example/weather/dao/QuoteDao.java
@@ -16,23 +16,20 @@ public class QuoteDao implements DAO<Quote> {
     @Getter(AccessLevel.PROTECTED)
     private EntityManager entityManager;
 
-    private Class getEntityClass() {
-        ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
-        return (Class) type.getActualTypeArguments()[0];
-    }
+
 
     @Override
     public Quote getById(Integer id) {
         EntityManager entityManager = getEntityManager();
-        Quote quote = (Quote) entityManager.find(getEntityClass(), id);
+        Quote quote = (Quote) entityManager.find(Quote.class, id);
         if (quote == null) {
-            throw new IllegalArgumentException(getEntityClass().getSimpleName() + " with id " + id + " is not found!");
+            throw new IllegalArgumentException(Quote.class.getSimpleName() + " with id " + id + " is not found!");
         }
         return quote;
     }
 
     @Override
     public Integer getCount() {
-        return Integer.parseInt(entityManager.createQuery("SELECT COUNT(q) FROM quotes q").getSingleResult().toString());
+        return Integer.parseInt(entityManager.createQuery("SELECT COUNT(*) FROM Quote quote").getSingleResult().toString());
     }
 }
